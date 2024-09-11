bc49b308fe363942d3df58ce6c007d24f93f9d6a
MarcosLaine <marcospslaine@gmail.com>
2024-09-11 17:52:19 -0300

    "Updated arithmetic operations in programa5.asm: changed subtraction to addition in two lines, and modified constants and shift amounts."

--- programa5.asm
+++ programa5.asm
@@ -13,9 +13,9 @@ slli t2, t0, 4 #t2 = t0 << 4 é o mesmo de dizer: t2 = 3 * 2^4 = 3*16
 sub t2, t2, t3  #t2 = t2 - 1 é o mesmo de dizer: t2 =  3*16 - 3 = 48 - 3 = 45 = x*15 =  3*15 = 45
 
 slli t5, t1, 6  #t5 = t1 << 6 é o mesmo de dizer: t5 =  4 * 2^6 = 4*64 = 256
-sub t5, t5, t6   #t5 = t5 - 12 é o mesmo de dizer: t5 =  256 - 12 = 244 = y*67 = 4*67 = 244
+addi t5, t5, 12   #t5 = t5 - 12 é o mesmo de dizer: t5 =  256 + 12 =  268 = y*67 = 4*67 = 268
 
-add t0, t5, t2   #t0 = t5 + t2 é o mesmo de dizer: t0 =  244 + 45 = 289 = 15*x + 67*y = 289
-slli t6, t0, 2   #t6 = t0 << 2  é o mesmo de dizer: t6 =  289 * 2^2 =  289 * 4 = 1156
+add t0, t5, t2   #t0 = t5 + t2 é o mesmo de dizer: t0 =    268 + 45 = 313 = 15*x + 67*y = 15* 3 + 67*4 = 313
+slli t6, t0, 2   #t6 = t0 << 2  é o mesmo de dizer:   t6 =  313 * 2^2 = 313 * 4 = 1252 
 
 nop

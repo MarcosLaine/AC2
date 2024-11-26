0c4d9deb4196a8f24bb888e7166886d3f671bf35
marcoslaine <marcospslaine@gmail.com>
2024-11-26 14:32:14 -0300

    add college work

---
+++ TrabAC2/fatIte.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+__uint128_t fatorial (__uint128_t n) {
+    __uint128_t result = 1;
+    for (__uint128_t i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+void print_uint128(__uint128_t n) {
+    if (n > UINT64_MAX) {
+        unsigned long long high = n / 10000000000000000000ULL;
+        unsigned long long low = n % 10000000000000000000ULL;
+        printf("%llu%019llu\n", high, low);
+    } else {
+        printf("%llu\n", (unsigned long long)n);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    __uint128_t n = 0;
+    char input[40];
+    scanf("%39s", input);
+    n = strtoull(input, NULL, 10);
+    __uint128_t result = fatorial(n);
+    print_uint128(result);
+    return 0;  
+}
